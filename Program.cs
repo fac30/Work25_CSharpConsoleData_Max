bc7ca5c5e138fb2da98ccc621fd5947aaaab17db
maxitect <maxime.downe@gmail.com>
2024-11-08 01:27:23 +0000

    feat: challenge to find, extract and replace module 5

--- Program.cs
+++ Program.cs
@@ -1,8 +1,16 @@
-﻿string data = "12345John Smith          5000  3  ";
-string updatedData = data.Remove(5, 20);
-Console.WriteLine(updatedData);
+﻿const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
 
-string message = "This--is--ex-amp-le--da-ta";
-message = message.Replace("--", " ");
-message = message.Replace("-", "");
-Console.WriteLine(message);
+string quantitySearch = "<span>";
+int quantityStart = input.IndexOf(quantitySearch) + quantitySearch.Length;
+int quantityLength = input.IndexOf("</span>") - quantityStart;
+string quantity = input.Substring(quantityStart, quantityLength);
+
+string outputSearch = "<div>";
+int outputStart = input.IndexOf("<div>") + outputSearch.Length;
+int outputLength = input.IndexOf("</div>") - outputStart;
+string output = input.Substring(outputStart, outputLength);
+
+output = output.Replace("&trade", "&reg");
+
+Console.WriteLine($"Quantity: {quantity}");
+Console.WriteLine($"Output: {output}");
